196bb4487e5ca387a6ee99d9719733081c18e38b
Jelly J <jellyj.@Jellys-Laptop.local>
2024-12-02 00:36:46 -0800

    added scorecard class

---
+++ lib/ScoreCard.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+/// This class will keep track of scores and log each chosen score
+/// after each dice roll.
+
+class ScoreCard{
+  /// Upper section of score card
+  late int ace;
+  late int twos;
+  late int threes;
+  late int fours;
+  late int fives;
+  late int sixes;
+  /// Lower section of score card
+  late int nOfAKind_3;
+  late int nOfAKind_4;
+  late int fullHouse;
+  late int smallStraight;
+  late int largeStraight;
+  late int YAHTZEE;
+  late int chance;
+  /// Totals
+  late int upperSectionTotal;
+  late int lowerSectionTotal;
+  late int grandTotal;
+
+  
+
+
+
+
+}
