8fbb54f400b8fe7b4dc462a3205143745ac78644
Jelly J <jellyj.@Jellys-Laptop.local>
2024-12-01 21:17:48 -0800

    finished class Score

--- lib/Category.dart
+++ lib/Category.dart
@@ -6,14 +6,14 @@ import 'package:flutter/material.dart';
 /// 2. Determine all categories of roll
 /// 3. Return all values of vaild categories
 
-class Category{
-  late int score;
+class Score{
   List<int> dice = List.filled(5, 0);
   Map<String, int> listOfDuplicates = {};
 
-  /// Constructor for the Category class that takes in die values
+  /// Constructor for the Category class that creates dice list
+  /// and sets list of duplicates
   /// Param: list of dice values
-  Category(List<int> diceList){
+  Score(List<int> diceList){
     dice = List.from(diceList);
     setDuplicateList();
   }
@@ -34,10 +34,10 @@ class Category{
     bool kind_3 = listOfDuplicates.containsValue(3);
     bool kind_4 = listOfDuplicates.containsValue(4);
     if(num == 3 && (kind_3 || kind_4)){
-      return sumUsingLoop(dice);
+      return sumOf(dice);
     }
     else if(num == 4 && kind_4){
-      return sumUsingLoop(dice);
+      return sumOf(dice);
     }
     else{
       return 0;
@@ -45,34 +45,86 @@ class Category{
   }
 
   /// Full House: three of one number and two of another
-  /// Returns 25 points
+  /// Return: scores of 0 or 25
   int fullHouse(){
-    return score;
+    bool kind_3 = listOfDuplicates.containsValue(3);
+    bool kind_2 = listOfDuplicates.containsValue(2);
+    if(kind_3 && kind_2){
+      return 25;
+    }
+    else{
+      return 0;
+    }
   }
 
   /// Small Straight: any sequence of 4 numbers (ex: 2,3,4,5)
-  /// Returns 30 points
+  /// list of small straights:
+  /// - 1, 2, 3, 4
+  /// - 2, 3, 4, 5
+  /// - 3, 4, 5, 6
+  /// Return: scores of 0 or 30
   int smallStraight(){
-    return score;
+    List<int> sequence1 = [1, 2, 3, 4];
+    List<int> sequence2 = [2, 3, 4, 5];
+    List<int> sequence3 = [3, 4, 5, 6];
+    bool list1 = containsSequence(dice, sequence1);
+    bool list2 = containsSequence(dice, sequence2);
+    bool list3 = containsSequence(dice, sequence3);
+
+    if(list1 || list2 || list3){
+      return 30;
+    }
+    else{
+      return 0;
+    }
+
   }
 
   /// Large Straight: any sequence of 5 numbers (ex: 2,3,4,5,6)
-  /// Returns 40 points
+  /// list of large straights:
+  /// - 1, 2, 3, 4, 5
+  /// - 2, 3, 4, 5, 6
+  /// Return: scores of 0 or 40
   int largeStraight(){
-    return score;
+    List<int> sequence1 = [1, 2, 3, 4, 5];
+    List<int> sequence2 = [2, 3, 4, 5, 6];
+    bool list1 = containsSequence(dice, sequence1);
+    bool list2 = containsSequence(dice, sequence2);
+
+    if(list1 || list2){
+      return 40;
+    }
+    else{
+      return 0;
+    }
+
   }
 
   /// YAHTZEE: 5 of the same number
   /// First YAHTZEE is worth 50 points
   /// Additional YAHTZEE is worth 100 points
-  int YAHTZEE(){
-    return score;
+  /// Param: the number of yahtzees already won by player
+  /// Return: scores of 0, 50, 100
+  int YAHTZEE(int yahtzee){
+    bool kind_5 = listOfDuplicates.containsValue(5);
+
+    if(kind_5){
+      if(yahtzee == 0) {
+        return 50;
+      }
+      else {
+        return 100;
+      }
+    }
+    else{
+      return 0;
+    }
   }
 
   /// Chance: any combination of dice
   /// Returns sum total of dice
   int chance(){
-    return score;
+    return sumOf(dice);
   }
 
   /// Checks for duplicates of a specific num (1-6) in the list, dice
@@ -101,12 +153,24 @@ class Category{
 
   /// Sum of a list of numbers
   /// Returns: sum
-  int sumUsingLoop(List<int> numbers) {
+  int sumOf(List<int> numbers) {
     int sum = 0;
     for (int number in numbers) {
       sum += number;
     }
     return sum;
+  }
+
+  /// Method for detecting sequences in a list
+  /// Returns: boolean value
+  bool containsSequence(List<int> list, List<int> sequence) {
+    for (int i = 0; i <= list.length - sequence.length; i++) {
+      if (list.sublist(i, i + sequence.length).every((element) =>
+                                      element == sequence[element - i])) {
+        return true;
+      }
+    }
+    return false;
   }
 
 }
