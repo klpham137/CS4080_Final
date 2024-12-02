ff9aa73a8e1f60126ab0abe2022aff2d2f54d786
Katie Pham <klpham137@gmail.com>
2024-12-01 23:43:38 -0800

    made dice UI the landing page and deleted files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Dice.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Yahtzee Game',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,100 +23,87 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Dice(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Dice extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _DiceState createState() => _DiceState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DiceState extends State<Dice> {
+  final Random _random = Random();
+  List<int> _diceValues = [1, 1, 1, 1, 1];
 
-  void _incrementCounter() {
+  void _rollDice() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _diceValues = List.generate(5, (_) => _random.nextInt(6) + 1);
     });
+  }
+
+  Widget _buildDice(int value) {
+    return Flexible(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Image.asset(
+          'assets/dice$value.png',
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Yahtzee Game'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          double diceSize = constraints.maxWidth / 6; // adjust dice size to fit on screen
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // top row has 3 dice
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: _diceValues
+                      .sublist(0, 3) // first 3 dice values
+                      .map((value) => SizedBox(
+                    width: diceSize,
+                    height: diceSize,
+                    child: _buildDice(value),
+                  ))
+                      .toList(),
+                ),
+                // bottom row has 2 dice
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: _diceValues
+                      .sublist(3, 5)
+                      .map((value) => SizedBox(
+                    width: diceSize,
+                    height: diceSize,
+                    child: _buildDice(value),
+                  ))
+                      .toList(),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _rollDice,
+                  child: Text('Roll'),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Navigate to Dice Page
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => Dice()),
           );
         },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
